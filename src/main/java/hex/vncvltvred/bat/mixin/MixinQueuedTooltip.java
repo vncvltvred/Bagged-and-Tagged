785b12e74efc939a337380d9329af684408a9af4
vncvltvred <vncvltvred@proton.me>
2024-08-06 02:54:51 +0000

    reduce
    reduce

--- src/main/java/hex/vncvltvred/bat/mixin/MixinQueuedTooltip.java
+++ src/main/java/hex/vncvltvred/bat/mixin/MixinQueuedTooltip.java
@@ -14,8 +14,6 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Stream;
 
 @SuppressWarnings("UnstableApiUsage")
 @Mixin(QueuedTooltip.class)
@@ -24,14 +22,13 @@ public abstract class MixinQueuedTooltip
     @Inject(method = "add(Lnet/minecraft/text/Text;)Lme/shedaniel/rei/api/client/gui/widgets/Tooltip;", at = @At("TAIL"))
     private void appendTags(Text text, CallbackInfoReturnable<Tooltip> cir)
     {
-        Stream<TagKey<?>> tags = ((QueuedTooltipInvoker)this).invokeGetContextStack().getTagsFor();
-        List<TagKey<?>> tagsList = tags.toList();
+        List<TagKey<?>> tags = ((QueuedTooltipInvoker)this).invokeGetContextStack().getTagsFor().toList();
 
-        if(tagsList.isEmpty()) return;
+        if(tags.isEmpty()) return;
 
         MinecraftClient mc = MinecraftClient.getInstance();
 
-        List<QueuedTooltip.TooltipEntryImpl> entryList = new java.util.ArrayList<>(tagsList
+        List<QueuedTooltip.TooltipEntryImpl> entryList = new java.util.ArrayList<>(tags
                 .stream()
                 .map(TagKey::toString)
                 .map(entry -> entry.substring(entry.contains("minecraft:item") ? 24 : 25, entry.length() - 1))
